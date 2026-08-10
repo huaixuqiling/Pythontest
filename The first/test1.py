620af71323d55be2906eeeadd9febf6302922441
HuaiXuQiLing <cqwm@qq.com>
2026-08-10 09:47:23 +0800

    Changes

--- The first/test1.py
+++ The first/test1.py
@@ -1,4 +1,4 @@
-# 这是一个注释 输出一些东西 CTRL+/
+# 这是一个注释 输出一些东西 CTRL+？
 print("Hello World")
 print("Hello Python")
 exit()
--- The first/test2.py
+++ The first/test2.py
@@ -1,4 +1,4 @@
-# 字面量写法
+字面量写法
 print(100)          #整数
 print(3.14)        #小数
 print(True)        #布尔
@@ -10,3 +10,23 @@ print(None)             #空值
 #布尔本质也是整数类型
 print(True+1)
 print(False-1)
+
+
+#变量  Py是一种动态语言 一个变量可以存储不同类型的数据
+num=114.1
+print(num)
+
+num=115
+print(num)
+
+num=num+1
+print(num)
+
+num="OK"
+print(num)
+
+#案例  一次定义多个变量
+base=20.7  #基础播放
+incr=50  #每月新增
+print("未来第一个月播放量：",base+incr)
+print("未来第二个月播放量：",base+incr*2)
