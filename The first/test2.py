900067762fdfd016c1cc0d21b889d05215b09eb5
HuaiXuQiLing <cqwm@qq.com>
2026-08-10 09:35:25 +0800

    Changes

---
+++ The first/test2.py
@@ -0,0 +1,12 @@
+# 字面量写法
+print(100)          #整数
+print(3.14)        #小数
+print(True)        #布尔
+print(False)        #BOOL
+print("hello world")    #字符串
+print(None)             #空值
+
+
+#布尔本质也是整数类型
+print(True+1)
+print(False-1)
