d7d4ce7e909315628d84cd6d42f603da435016c5
HuaiXuQiLing <cqwm@qq.com>
2026-08-10 10:09:14 +0800

    Changes

---
+++ The first/test3.py
@@ -0,0 +1,20 @@
+#常见数据类型
+print("Hello Python")
+print(type("Hello Python"))
+
+print(type(10))
+print(type(3.14))
+print(type(True))
+print(type(False))
+print(type(None))
+
+num=-100.0
+print(num)
+print(type(num))
+
+#isinstance判定数据类型
+num=-100.0
+print(num)
+print(isinstance(num,int))
+print(isinstance(num,float))
+print(isinstance(num,bool))
