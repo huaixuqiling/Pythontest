ea0f84c7c5c765b904ab04a92af0cd6c22e0ab1f
HuaiXuQiLing <cqwm@qq.com>
2026-08-11 09:45:15 +0800

    Changes

--- The first/test8.py
+++ The first/test8.py
@@ -76,3 +76,76 @@ for i in range(n):
         else:
             print("□", end=" ")
     print()
+
+#案例
+while True:
+    uesrname = input("请输入您的用户名:")
+    password = input("请输入您的密码:")
+    if uesrname=="admin" and password=="666888":
+        print("登陆成功，进入B站首页")
+        break
+    elif uesrname=="zahngsan" and password=="123456":
+        print("登陆成功，进入B站首页")
+        break
+    elif uesrname=="taoge" and password=="888666":
+        print("登陆成功，进入B站首页")
+        break
+    elif uesrname=="" or password=="":
+        print("同户名和密码不能为空")
+    else:
+        print("用户名或密码错误，请重新输入:")
+
+#练习
+
+for i in range(5):
+    uesrname = input("请输入您的用户名:")
+    password = input("请输入您的密码:")
+    if uesrname=="" or password=="":
+        print("同户名和密码不能为空")
+        continue
+    if uesrname=="admin" and password=="666888":
+        print("登陆成功，进入B站首页")
+        break
+    elif uesrname=="zahngsan" and password=="123456":
+        print("登陆成功，进入B站首页")
+        break
+    elif uesrname=="taoge" and password=="888666":
+        print("登陆成功，进入B站首页")
+        break
+    else:
+        print("用户名或密码错误，请重新输入:")
+
+#案例2 猜数字
+
+import random
+random_number=random.randint(1,100)
+while True:
+    num=int(input("请输入您的数字:"))
+    if num==random_number:
+        print("猜对了！")
+        break
+    elif num>random_number:
+        print("输入的数字大了")
+    elif num<random_number:
+        print("输入的数字小了")
+    else:
+        print("输入错误，请重新输入")
+
+#练习1
+n=0
+for i in range(1,1001):
+    if i%5==0:
+        n=n+i
+print(f"1000以内所有5倍数字累加起来之和为{n}")
+
+#练习2 统计字符串“akiwksjakdiklowiqaamnvbamvaxnsjdsjkaaxkjd”中有多少个a和k
+
+msg="akiwksjakdiklowiqaamnvbamvaxnsjdsjkaaxkjd"
+numa=0
+numk=0
+for i in msg:
+    if i=="a":
+        numa+=1
+    elif i=="k":
+        numk+=1
+print(f"字符串中a的个数为{numa}，k的个数为{numk}")
